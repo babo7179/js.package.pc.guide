1e1f3e47ba51ba3b506eea20f1873be6645d2fa0
jeroskim <jazz_fx@mz.co.kr>
2014-09-05 17:55:36 +0900

    update

--- gruntfile.js
+++ gruntfile.js
@@ -3,37 +3,65 @@ module.exports = function(grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			options: {
+				separator: ';',
+				stripBanners: true,
+      			banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */'
+			},
+			jquery_plugins: {
+				src: [
+					'<%= pkg.path.plugin %>/jquery.navigate.js', 
+					'<%= pkg.path.plugin %>/jquery.dotdotdot.js', 
+					'<%= pkg.path.plugin %>/jquery.form.js', 
+					'<%= pkg.path.plugin %>/jquery.validate.js'
+				],
+				dest: '<%= pkg.path.plugin %>/jquery.plugin.pkg.js'
+			},
+			dist : {
+				src: [
+					// '<%= pkg.path.jrx %>/jrx.js', 
+					'<%= pkg.path.jrx %>/jrx.init.js'
+				],
+				dest: '<%= pkg.path.jrx %>/jrx.full.js'
+			}
+		},
 		uglify: {
-			// options: {
-				// banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-			// },
-			utils: {
-				src: 'scripts/jquery/jquery.utils.js',
-				dest: 'scripts/jquery/jquery.utils.min.js'
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+				report: 'min'
+			},
+			jrx: {
+				options:{
+					sourceMap : true,
+					sourceMapName:'<%= pkg.path.jrx %>/jrx.full.min.map'
+				},
+				files : {
+					'<%= pkg.path.jrx %>/jrx.full.min.js' : [
+						'<%= pkg.path.jrx %>/jrx.full.js'
+					]
+				}
 			},
 			form: {
-				src: 'scripts/jquery/plugin/jquery.form.js',
-				dest: 'scripts/jquery/plugin/jquery.form.min.js'
+				src: '<%= pkg.path.plugin %>/jquery.form.js',
+				dest: '<%= pkg.path.plugin %>/jquery.form.min.js'
 			},
 			layer: {
-				src: 'scripts/jquery/plugin/jquery.layer.js',
-				dest: 'scripts/jquery/plugin/jquery.layer.min.js'
+				src: '<%= pkg.path.plugin %>/jquery.layer.js',
+				dest: '<%= pkg.path.plugin %>/jquery.layer.min.js'
 			},
-			admin_plugin_package : {
+			jquery_plugins : {
 				files : {
-					'scripts/jquery/plugin/jquery.plugin.pkg.js' : [
-						'scripts/jquery/plugin/jquery.navigate.js',
-						'scripts/jquery/plugin/jquery.dotdotdot.js',
-						'scripts/jquery/plugin/jquery.form.js',
-						'scripts/jquery/plugin/jquery.validate.js',
+					'<%= pkg.path.plugin %>/jquery.plugin.pkg.min.js' : [
+						'<%= pkg.path.plugin %>/jquery.plugin.pkg.js'
 					]
 				}
 			}
 		},
 		jsdoc : {
 			dist : {
-				src : ['scripts/jquery/plugin/jquery.plugin.sample.js'],
-				// src : ['scripts/jquery/plugin/jquery.plugin.sample.js', 'scripts/jquery/jquery.utils.js'],
+				src : ['<%= pkg.path.plugin %>/jquery.plugin.sample.js'],
+				// src : ['<%= pkg.path.plugin %>/jquery.plugin.sample.js', 'scripts/jquery/jquery.utils.js'],
 				options : {destination : 'doc'}
 			}
 		},
@@ -47,17 +75,23 @@ module.exports = function(grunt) {
 				}
 			}
 		}
+		
 	});
 	
 	// Load the plugin that provides the "uglify" task.
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-jsdoc');
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	
 	// Default task(s).
-	grunt.registerTask('default', ['uglify', 'jsdoc']);
+	grunt.registerTask('default', ['concat', 'uglify', 'jsdoc']);
 	
 	grunt.registerTask('doc', ['jsdoc']);
 	
-	grunt.registerTask('min', ['uglify']);
+	grunt.registerTask('min', ['concat', 'uglify']);
+	
+	// grunt.registerTask('concat', ['concat']);
+	
+	// grunt.registerTask('less', ['less']);
 };
